1015c41b8bf3006a6f36fcc1565ee2cefcc788f0
kelvinhenri99 <kelvin5henri@gmail.com>
2024-11-26 17:59:05 -0300

    feat: ajusting vite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-// https://vitejs.dev/config/
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
     plugins: [
         vue(),
     ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)) // Alias @ apontando para src
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
     build: {
-        outDir: 'dist', // Define o diretório de saída para o build
-        assetsDir: 'assets', // Define o diretório de assets dentro de dist
-        sourcemap: true, // Habilita a geração de sourcemaps, útil para depuração
+        outDir: 'dist',
+        sourcemap: false
     },
-    server: {
-        port: 3000, // Porta para o servidor de desenvolvimento local
-    }
-});
+})
